643fc11a518e3d3fa194ad9f4db76b638d7052b3
Mahadi9664 <momahadi9664@gmail.com>
2024-06-30 10:57:25 +0600

    randomly puts 2's now

--- 2048.py
+++ 2048.py
@@ -10,19 +10,20 @@ WIDTH, HEIGHT = 800, 800
 ROWS = 4
 COLS = 4
 
-RECT_HEIGHT = HEIGHT//ROWS
-RECT_WIDTH = WIDTH//ROWS
+RECT_HEIGHT = HEIGHT // ROWS
+RECT_WIDTH = WIDTH // COLS
 
 OUTLINE_COLOR = (187, 173, 160)
 OUTLINE_THICKNESS = 7
 BACKGROUND_COLOR = (205, 192, 180)
 FONT_COLOR = (119, 110, 101)
 
-FONT = pygame.font.SysFont("helvetica", 60, bold= True)
+FONT = pygame.font.SysFont("helvetica", 60, bold=True)
 MOVE_VEL = 20
 
 WINDOW = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("2048")
+
 
 class Tile:
     COLORS = [
@@ -37,7 +38,7 @@ class Tile:
         (236, 202, 80),
     ]
 
-    def __init__(self,value,row,col):
+    def __init__(self, value, row, col):
         self.value = value
         self.row = row
         self.col = col
@@ -45,49 +46,66 @@ class Tile:
         self.y = row * RECT_HEIGHT
 
     def get_color(self):
-        color_index = int(math.log2(self.value))-1
+        color_index = int(math.log2(self.value)) - 1
         color = self.COLORS[color_index]
         return color
-    
+
     def draw(self, window):
         color = self.get_color()
         pygame.draw.rect(window, color, (self.x, self.y, RECT_WIDTH, RECT_HEIGHT))
 
-        text = FONT.render(str(self.value),1,FONT_COLOR)
+        text = FONT.render(str(self.value), 1, FONT_COLOR)
         window.blit(
             text,
             (
-                self.x + (RECT_WIDTH/2-text.get_width()/2),
-                self.y + (RECT_HEIGHT/2-text.get_height()/2)
+                self.x + (RECT_WIDTH / 2 - text.get_width() / 2),
+                self.y + (RECT_HEIGHT / 2 - text.get_height() / 2),
             ),
         )
-        
-    def set_pos(self):
-        pass
-    
-    def move(self, delta):
-        pass
 
+    def set_pos(self, row, col):
+        self.row = row
+        self.col = col
+        self.x = col * RECT_WIDTH
+        self.y = row * RECT_HEIGHT
 
-def draw_grid(window):
-    for row in range(ROWS):
-        y = row * RECT_HEIGHT
-        pygame.draw.line(window, OUTLINE_COLOR, (0,y), (WIDTH, y), OUTLINE_THICKNESS)
-        
-    for cols in range(COLS):
-        x = cols * RECT_WIDTH
-        pygame.draw.line(window, OUTLINE_COLOR, (x,0), (x, HEIGHT), OUTLINE_THICKNESS)
-
-
-    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+    def move(self, delta_row, delta_col):
+        self.set_pos(self.row + delta_row, self.col + delta_col)
 
 def draw(window, tiles):
     window.fill(BACKGROUND_COLOR)
 
-    for tile in tiles.values:
-        tile.draw()
+    for tile in tiles.values():
+        tile.draw(window)
     draw_grid(window)
     pygame.display.update()
+
+def draw_grid(window):
+    for row in range(ROWS):
+        y = row * RECT_HEIGHT
+        pygame.draw.line(window, OUTLINE_COLOR, (0, y), (WIDTH, y), OUTLINE_THICKNESS)
+
+    for col in range(COLS):
+        x = col * RECT_WIDTH
+        pygame.draw.line(window, OUTLINE_COLOR, (x, 0), (x, HEIGHT), OUTLINE_THICKNESS)
+
+    pygame.draw.rect(window, OUTLINE_COLOR, (0, 0, WIDTH, HEIGHT), OUTLINE_THICKNESS)
+
+def get_random_pos(tiles):
+    while True:
+        row = random.randrange(0, ROWS)
+        col = random.randrange(0, COLS)
+        if f"{row}{col}" not in tiles:
+            break
+    return row, col
+
+
+def generate_tiles():
+    tiles = {}
+    for _ in range(2):
+        row, col = get_random_pos(tiles)
+        tiles[f"{row}{col}"] = Tile(2, row, col)
+    return tiles
 
 
 
@@ -96,20 +114,20 @@ def main(window):
     clock = pygame.time.Clock()
     run = True
 
-
+    tiles = generate_tiles()
 
     while run:
         clock.tick(FPS)
 
         for event in pygame.event.get():
-            print(event.type)
             if event.type == pygame.QUIT:
                 run = False
-                break 
+                break
 
-        draw(window)
+        draw(window, tiles)
 
     pygame.quit()
+
 
 if __name__ == "__main__":
     main(WINDOW)
